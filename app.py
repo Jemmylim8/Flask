3a50fa6f0300cc08aa43fcb9f74946598a4f36ef
Jemmylim8 <jemlimthesec@gmail.com>
2025-04-29 10:27:15 +0800

    second commit

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello from Flask on Heroku! JEMMMMYYY"
+    return "Hello from Flask on Heroku! JEMMMMYYY LIIIM"
 
 if __name__ == "__main__":
     app.run(debug=True)
