5facad50d6b1497c8780a0b8ccbe62def67126f9
Jemmylim8 <jemlimthesec@gmail.com>
2025-05-08 09:30:35 +0800

    first commit

--- app.py
+++ app.py
@@ -5,6 +5,6 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello from Flask on Heroku! JEMMMMYYY LIIIM"
+    return "Jemmy Lim II \n 1st 25 \n ITE 322 \n Semi-Final Exam"
 port = int(os.environ.get("PORT", 5000))
 app.run(host='0.0.0.0' , port=port)
